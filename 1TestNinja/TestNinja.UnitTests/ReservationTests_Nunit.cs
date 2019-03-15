62b4e8680cd1349925eca083857efa44bb505caa
Buddhika85 <buddhika2006system@gmail.com>
2019-03-15 11:42:21 +0530

    N unit

---
+++ 1TestNinja/TestNinja.UnitTests/ReservationTests_Nunit.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests_Nunit
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_RetunsTrue()
+        {
+            // arrange
+            var reservation = new Reservation();
+
+            // act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // assert
+            //Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsMadeByUser_RetunsTrue()
+        {
+            // arrange
+            var reservation = new Reservation { MadeBy = new User { IsAdmin = true } };
+
+            // act
+            var result = reservation.CanBeCancelledBy(reservation.MadeBy);
+
+            // assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsNotAdmin_RetunsFalse()
+        {
+            // arrange
+            var reservation = new Reservation();
+
+            // act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            // assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserIsNotMadeByUser_RetunsFalse()
+        {
+            // arrange
+            var reservation = new Reservation { MadeBy = new User { IsAdmin = true } };
+
+            // act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            // assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
